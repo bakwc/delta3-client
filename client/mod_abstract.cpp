19fcc48780e75ef7af184222e3d50769fcafbcec
Cool-T <bub4z0r@gmail.com>
2012-07-24 03:41:08 +0400

    added abstracts from develop

---
+++ client/mod_abstract.cpp
@@ -0,0 +1,16 @@
+#include "mod_abstract.h"
+//------------------------------------------------------------------------------
+delta3::mod_abstract::mod_abstract(QObject *parent = 0, quint16 adminId = 0)
+    : QObject(parent),
+      _adminId(adminId)
+{
+
+}
+//------------------------------------------------------------------------------
+/*
+delta3::mod_abstract::getAdminId()                          const
+{
+    return _adminId;
+}
+*/
+//------------------------------------------------------------------------------
