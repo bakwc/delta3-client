e6a69ad5996193554093a6a8728e6d78b8240ce4
Alexandr Nikolaichev <alexandr.nikolaichev@gmail.com>
2012-08-02 11:12:39 +0400

    Added command prompt to linux terminal

--- client/mod_telnet.cpp
+++ client/mod_telnet.cpp
@@ -9,34 +9,23 @@ ModTelnet::ModTelnet(qint16 adminId, Client *client)
         _protocol = new QProcess(this);
     #ifdef Q_WS_X11                             // linux desktop
         //constructing command prompt
-        QByteArray username,hostname,cwd;
+        QByteArray username,hostname;
         QProcess username_proc;
         QProcess hostname_proc;
-        QProcess cwd_proc;
         username_proc.start("whoami");
         username_proc.waitForFinished();
         username = username_proc.readAll();
-        //qDebug() << username;
         hostname_proc.start("hostname");
         hostname_proc.waitForFinished();
         hostname = hostname_proc.readAll();
-        //qDebug() << hostname;
-        cwd_proc.start("pwd");
-        cwd_proc.waitForFinished();
-        cwd = cwd_proc.readAll();
         username_proc.close();
         hostname_proc.close();
-        cwd_proc.close();
         prompt = QString::fromUtf8(username);
         prompt.chop(1);
         prompt.append("@");
         prompt.append(hostname);
         prompt.chop(1);
-        prompt.append(" ");
-        prompt.append(cwd);
-        prompt.chop(1);
-        prompt.append("$ ");
-        //qDebug() << "Prompt: " << prompt;
+        prompt.append(" $ ");
         _protocol->start("/bin/sh");
     #elif defined(Q_WS_MAC)                     // darwin
         _protocol->start("/bin/bash");
